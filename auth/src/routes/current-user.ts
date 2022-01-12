ad681035fb6971c23ab7599ee9d585472177d0d9
Elayade ismail <elayadeismail@gmail.com>
2022-01-12 16:16:06 +0100

    add auth mongo depl

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,7 +3,6 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/api/users/currentuser', async (req, res) => {
-  console.log('Daaata', req.body);
   res.send('Hi there');
 });
 
