edfdb69b54bef4bfdb4a29c31f50deb91648c691
Elayade ismail <elayadeismail@gmail.com>
2022-01-13 09:44:42 +0100

    hash password before storing the user

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { PasswordManager } from '../services/password';
 
 // An interface that describes the properties
 // that are required to create a new User
@@ -32,6 +33,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await PasswordManager.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -3,7 +3,7 @@ import { promisify } from 'util';
 
 const scryptAsync = promisify(scrypt);
 
-export class Password {
+export class PasswordManager {
   static async toHash(password: string) {
     const salt = randomBytes(8).toString('hex');
     const buf = (await scryptAsync(password, salt, 64)) as Buffer;
@@ -11,5 +11,9 @@ export class Password {
     return `${buf.toString('hex')}.${salt}`;
   }
 
-  static compare(storedPassword: string, suppliedPassword: string) {}
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.');
+    const buff = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+    return buff.toString('hex') === hashedPassword;
+  }
 }
