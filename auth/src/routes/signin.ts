8c33b0d8e4bd2af20456b65959941dc95c7bedcc
Elayade ismail <elayadeismail@gmail.com>
2022-01-22 17:25:47 +0100

    add validation request middelware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidatorError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', async (req, res) => {
-  res.send('Hi there');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('you must provide a password'),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidatorError(errors.array());
+    }
+    res.send('Hi there');
+  }
+);
 
 export { router as signInRouter };
