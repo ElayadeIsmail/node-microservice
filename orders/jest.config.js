05ff4e8ea8ab129aa0f149a38b360f827d9dccd8
ElayadeIsmail <elayadeismail@gmail.com>
2022-03-17 14:57:53 +0100

    add jest config

---
+++ orders/jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // setupFilesAfterEnv: ['./src/test/setup.ts'],
+  testTimeout: 100000,
+};
