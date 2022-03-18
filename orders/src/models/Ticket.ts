ea063f6542d10990e26b4ad7cedb4e838c9408a9
ElayadeIsmail <elayadeismail@gmail.com>
2022-03-18 09:21:51 +0100

    add ticket and order models

---
+++ orders/src/models/Ticket.ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+interface TicketAttrs {
+  userId: string;
+  title: string;
+  price: number;
+}
+export interface TicketDoc extends mongoose.Document {
+  userId: string;
+  title: string;
+  price: number;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket };
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,23 @@
+import { requireAuth, validateRequest } from '@eitickets/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express();
 
-router.post('/api/orders', async (req: Request, res: Response) => {});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .isMongoId()
+      .withMessage('ticketID must be valid'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as createOrderRouter };
