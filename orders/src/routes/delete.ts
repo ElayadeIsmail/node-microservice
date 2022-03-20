dc0cd641b88dd8630a5bef1b2d176dd05237a311
ElayadeIsmail <elayadeismail@gmail.com>
2022-03-20 12:24:57 +0100

    add canceling order with test

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,10 +1,29 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+} from '@eitickets/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/Order';
 
 const router = express();
 
 router.delete(
   '/api/orders/:orderId',
-  async (req: Request, res: Response) => {}
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId);
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    res.status(204).send(order);
+  }
 );
 
 export { router as deleteOrderRouter };
