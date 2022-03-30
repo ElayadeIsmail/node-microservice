85db76e5c50a938eeb5d2bcdf0c5c49e93a3ad0f
ElayadeIsmail <elayadeismail@gmail.com>
2022-03-30 20:51:57 +0000

    add setup for expiration service

---
+++ expiration/node_modules/@eitickets/common/build/events/order-created-event.d.ts
@@ -0,0 +1,16 @@
+import { Subjects } from './subjects';
+import { OrderStatus } from './types/order-status';
+export interface OrderCreatedEvent {
+    subject: Subjects.OrderCreated;
+    data: {
+        id: string;
+        status: OrderStatus;
+        version: number;
+        userId: string;
+        expiresAt: string;
+        ticket: {
+            id: string;
+            price: number;
+        };
+    };
+}
---
+++ expiration/src/index.ts
@@ -0,0 +1,32 @@
+import { natsWrapper } from './nats-wrapper';
+
+const start = async () => {
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    // close nats
+    natsWrapper.client.on('close', () => {
+      console.log('Closing Nats ');
+      process.exit();
+    });
+    // CALL nats close function after trying to shut dow process
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
