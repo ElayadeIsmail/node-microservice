0f42bf55296e643835a47c6c472a66bc62cebb1d
ElayadeIsmail <elayadeismail@gmail.com>
2022-04-01 20:23:53 +0000

    add Order Model

---
+++ payments/src/models/Order.ts
@@ -0,0 +1,61 @@
+import { OrderStatus } from '@eitickets/common';
+import mongoose from 'mongoose';
+
+interface OrderAttrs {
+  id: string;
+  status: OrderStatus;
+  version: number;
+  userId: string;
+  price: number;
+}
+
+interface OrderDoc extends mongoose.Document {
+  status: OrderStatus;
+  version: number;
+  userId: string;
+  price: number;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: OrderStatus,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    status: attrs.status,
+    price: attrs.price,
+    userId: attrs.userId,
+    version: attrs.version,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
