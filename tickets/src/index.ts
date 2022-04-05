236898d0ea5d015481c4bc0e080c59f3fb3a91ac
ElayadeIsmail <elayadeismail@gmail.com>
2022-04-05 23:36:13 +0000

    add starting log to tickets service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,6 +5,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
+  console.log('Staaarting');
   if (!process.env.JWT_KEY) {
     throw new Error('Jwt must be defined');
   }
